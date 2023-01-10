1fcb2eb2602d8fb073e8ef57548a6f70bdeea41d
ganesh <ganeshsadhashivam@outlook.com>
2023-01-10 15:11:49 +0530

    favicons title added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,12 +2,16 @@ const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 
+//for registering new user
+
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
 
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
+
+//for Login
 
 const login = async (req, res) => {
   const { email, password } = req.body;
